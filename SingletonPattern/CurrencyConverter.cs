58906bb19c277759278a77196f1fdd5b51ddb3c2
yaseen nasr <yaseennasr1300@gmail.com>
2023-03-07 21:02:50 +0100

    SingletonPattern

---
+++ SingletonPattern/CurrencyConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class CurrencyConverter
+    {
+        private IEnumerable<ExchangeRate> _exchangeRates;
+        private CurrencyConverter()
+        {
+            LoadExchangeRates();
+        }
+        private static object _lock = new();
+        //Lazy initialization 
+        private static CurrencyConverter _instance;
+        public static CurrencyConverter Instance
+        {
+            get
+            {
+                //Double check
+                if (_instance is null) 
+                {
+                    lock (_lock)
+                    {
+                        if (_instance is null)
+                            _instance = new CurrencyConverter();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private void LoadExchangeRates()
+        {
+            Thread.Sleep(3000);
+            _exchangeRates = new[]
+            {
+                new ExchangeRate("USD","SAR",3.75m),
+                new ExchangeRate("USD","EGP",30.60m),
+                new ExchangeRate("SAR","EGP",8.16m),
+            };
+        }
+        public decimal Convert(string baseCurrency, string targetCurrency, decimal amount)
+        {
+            //Assum Happy bath without null checks
+            var exchangeRate = _exchangeRates.FirstOrDefault(rate => rate.BaseCurrency == baseCurrency && rate.TargetCurrency == targetCurrency);
+            return amount * exchangeRate!.Rate;
+        }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonPattern;
+while (true)
+{ 
+    Console.WriteLine("Enter base currency: ");
+    var basecurrency = Console.ReadLine();
+    Console.WriteLine("Enter target currency: ");
+    var targetcurrency = Console.ReadLine();
+    Console.WriteLine("Enter amount: ");
+    var amount = decimal.Parse(Console.ReadLine());
+    //var coverter = new CurrencyConverter();
+    var exchangeAmount = CurrencyConverter.Instance.Convert(basecurrency, targetcurrency, amount);
+    Console.WriteLine($"{amount} {basecurrency} = {exchangeAmount} {targetcurrency}");
+    Console.WriteLine("---------------------");
+}
