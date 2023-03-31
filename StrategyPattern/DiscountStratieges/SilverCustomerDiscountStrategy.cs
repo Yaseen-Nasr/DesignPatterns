56595a44f4ec11976b26956a91fb8f74cd3fdba0
yaseen nasr <yaseennasr1300@gmail.com>
2023-03-31 22:55:32 +0200

    Applay strategy pattern

---
+++ StrategyPattern/DiscountStratieges/SilverCustomerDiscountStrategy.cs
@@ -0,0 +1,9 @@
+﻿namespace StrategyPattern.DiscountStratieges
+{
+    internal class SilverCustomerDiscountStrategy : ICustomerDiscountStrategy
+    {
+        public double CalculateDiscount(double totalPrice)
+        {
+            return totalPrice >= 10000 ? .05 : 0;                }
+    }
+}
