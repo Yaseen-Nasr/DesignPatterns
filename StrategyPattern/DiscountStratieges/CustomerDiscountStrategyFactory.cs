cb6e88f61a2ecd555ac784836a99cfc52a483348
yaseen nasr <yaseennasr1300@gmail.com>
2023-04-01 14:51:50 +0200

    Apply simple factory pattern in StrategyPattern project when create new instance of ICustomerDiscountStrategy to centralizetion creation instance

---
+++ StrategyPattern/DiscountStratieges/CustomerDiscountStrategyFactory.cs
@@ -0,0 +1,19 @@
+﻿
+namespace StrategyPattern.DiscountStratieges
+{
+    internal class CustomerDiscountStrategyFactory
+    {
+        //centralizetion creating CustomerDiscountStrategy instance and reusability 
+        public ICustomerDiscountStrategy CreateCustomerDiscountStrategy(CustomerCategory category)
+        {
+           return category switch
+            {
+                CustomerCategory.New => new NewCustomerDiscountStrategy(),
+                CustomerCategory.Silver => new SilverCustomerDiscountStrategy(),
+                CustomerCategory.Gold => new GoldCustomerDiscountStrategy(),
+                _ => new NewCustomerDiscountStrategy()
+            }; 
+        }
+
+    }
+}
--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -16,19 +16,14 @@ while (true)
     double UnitPrice = double.Parse(Console.ReadLine()!);
     //assume happy path without any validation 
     var customer = customers.First(x => x.Id == customerId);
-
-    ICustomerDiscountStrategy discountStrategy =
-                             customer.CustomerCategory switch
-                             {
-                                 CustomerCategory.New => new NewCustomerDiscountStrategy(),
-                                 CustomerCategory.Silver => new SilverCustomerDiscountStrategy(),
-                                 CustomerCategory.Gold => new GoldCustomerDiscountStrategy(),
-                                 _ => new NewCustomerDiscountStrategy()
-                             };
+    //Using sample factory pattern
+    ICustomerDiscountStrategy discountStrategy =new CustomerDiscountStrategyFactory()
+                                        .CreateCustomerDiscountStrategy(customer.CustomerCategory);
+        
 
     var invoiceManager = new InvoiceManager();
     invoiceManager.SetDiscountStrategy(discountStrategy);
     Invoice invoice = invoiceManager.CreateInvoice(customer, quantity, UnitPrice);
-    //var invoice=
+     
     Console.WriteLine($"Invoice created for customer `{customer.Name}` with net price: {invoice.NetPrice}");
 }
